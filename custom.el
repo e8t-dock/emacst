e408e603595e7f03ac0d1b46a0afb2b5208eeb6e
Peter Lau <superpeterlau@outlook.com>
2022-01-10 19:05:54 +0800

    add custom.el

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("77c16fe29860c6fc12cde892d6e3051c740a7b3781c988aeb4df9743f4a24e0d" default))
+ '(org-agenda-files '("~/Desktop/app/orgapp/1.org"))
+ '(package-selected-packages
+   '(exec-path-from-shell go-mode helm-lsp lsp-mode magit use-package company dracula-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
