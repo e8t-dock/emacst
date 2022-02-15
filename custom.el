02f678e514292d7b9144ec335c5bcb79a87af791
Peter Lau <superpeterlau@outlook.com>
2022-02-15 13:29:27 +0800

    custom-set-variables

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("77c16fe29860c6fc12cde892d6e3051c740a7b3781c988aeb4df9743f4a24e0d" default))
+   '("c7000071e9302bee62fbe0072d53063da398887115ac27470d664f9859cdd41d" "77c16fe29860c6fc12cde892d6e3051c740a7b3781c988aeb4df9743f4a24e0d" default))
  '(org-agenda-files '("~/Desktop/app/orgapp/1.org"))
  '(package-selected-packages
    '(exec-path-from-shell go-mode helm-lsp lsp-mode magit use-package company dracula-theme)))
